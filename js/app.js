38c844bea3bd632d4b3b09bd0aa6881cc46fa8a6
DVs07 <aldopablo7@hotmail.com>
2025-01-28 20:03:05 -0300

    Se agrego la validacion al formulario de deudor y se muestra un mensaje si al menos un campo se encuentra vacio o si se agrego correctamente.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,57 @@
 const btnAgregar = document.getElementById('btn-agregar');
+const formulario = document.getElementById('formulario-deudor');
 
-btnAgregar.addEventListener('click', agregarDeudor);
+const alerta = document.createElement('div');
 
+const nombre = document.getElementById('nombre-deudor');
+const apellido = document.getElementById('apellido-deudor');
+const fecha = document.getElementById('fecha');
+const cantidad = document.getElementById('cantidad');
+const pagado = document.getElementById('pagado');
+
+eventListeners();
+
+function eventListeners(){
+
+    btnAgregar.addEventListener('click', agregarDeudor);
+}
 function agregarDeudor(e){
     e.preventDefault();
     console.log('Agregando deudor');
+    limpiarHtml(alerta);
+    validarDeudor();
+}
+
+function validarDeudor(){
+
+    if( nombre.value === '' || apellido.value === '' || fecha.value === '' || cantidad.value === '' || pagado.value === '' ){
+        mostrarAlerta('Todos los campos son obligatorios', 'error');
+        return;
+    } else {
+        mostrarAlerta('Agregado correctamente', 'correcto');
+    }
+
+}
+
+function mostrarAlerta(mensaje, tipo){
+    
+    
+    alerta.classList.add('text-center', 'alert', 'alert-danger', 'mt-4');
+    alerta.textContent = mensaje;
+
+    
+    if(tipo === 'correcto'){
+        alerta.classList.remove('alert-danger');
+        alerta.classList.add('alert-success');
+    }
+    formulario.appendChild(alerta);
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
+
+function limpiarHtml(elemento){
+    while(elemento.firstChild){
+        elemento.removeChild(elemento.firstChild);
+    }
 }
