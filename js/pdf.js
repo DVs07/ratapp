b42c5ac8ed4788d1eec6222535446cf671638a60
DVs07 <aldopablo7@hotmail.com>
2025-02-13 23:53:07 -0300

    Se hicieron cambios en como se muestra el PDF para poder descargarlo/imprimir.

--- js/pdf.js
+++ js/pdf.js
@@ -1,7 +1,9 @@
 // import jsPDF from "jspdf";
 
 
-export const divPDF = document.querySelector('#pdf');
+export const divPDF = document.querySelector('#main-pdf');
+export const headerPDF = document.querySelector('#header-pdf');
+export const footerPDF = document.querySelector('#footer-pdf');
 
 document.addEventListener("DOMContentLoaded", function () {
     const deudor = JSON.parse(localStorage.getItem("deudorPDF"));
@@ -13,15 +15,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const { nombre, apodo, fecha, cantidad, pagado } = deudor;
 
+    const titulo = document.createElement("h1");
+    titulo.classList.add("text-center", 'py-3','mb-4','border-bottom');
+    titulo.textContent = "Informe";
+    headerPDF.appendChild(titulo);
+
+    divPDF.classList.add("text-center");
     divPDF.innerHTML = `
-        <h1>Informe</h1>
         <p>Nombre: ${nombre}</p>
         <p>Apodo: ${apodo}</p>
         <p>Fecha Préstamo: ${fecha}</p>
         <p>Cantidad Prestada: ${cantidad}</p>
         <p>Pagado: ${pagado}</p>
-        <button id="btnDescargarPDF">Descargar PDF</button>
     `;
+
+    const btnDescargarPDF = document.createElement("button");
+    btnDescargarPDF.classList.add("btn", "btn-primary");
+    btnDescargarPDF.id = "btnDescargarPDF";
+    btnDescargarPDF.textContent = "Descargar PDF";
+    footerPDF.classList.add('flex', 'mt-4','justify-center','align-items-center');
+    footerPDF.appendChild(btnDescargarPDF);
 
     // Evento para descargar PDF con jsPDF
     // document.getElementById("btnDescargarPDF").addEventListener("click", function () {
